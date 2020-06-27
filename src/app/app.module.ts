cecb14905cb519dfe7e193d88906e892e1c4db82
sarasali757 <58108069+sarasali757@users.noreply.github.com>
2020-06-27 13:20:59 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ProductsComponent } from './products/products.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+
+import { ClientComponent } from './client/client.component';
+import { AddClientComponent} from './client/add-client/add-client.component';
+import { ClientsListComponent} from './client/clients-list/clients-list.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button';
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { ManualOrderComponent } from './manual-order/manual-order.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminNavbarComponent,
+    ManualOrderComponent,
+    ProductsComponent,
+    AddProductComponent,
+    ProductListComponent,
+    ClientComponent,
+    AddClientComponent,
+    ClientsListComponent,
+
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatBadgeModule,
+    MatButtonToggleModule,
+    MatTooltipModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/manual-order/manual-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-order',
+  templateUrl: './manual-order.component.html',
+  styleUrls: ['./manual-order.component.css']
+})
+export class ManualOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
